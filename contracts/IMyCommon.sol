a68ad170c35ad1cb32bb0a1be8cb62ab849ede7d
acstevens01 <98523987+acstevens01@users.noreply.github.com>
2023-05-23 18:40:34 +0100

    Deletion

--- contracts/IMyCommon.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-
-interface IMyCommon {
-    function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
-    function transferFrom(address from, address to, uint value) external returns (bool);
-}
--- web/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from '@/App.vue'
-import store from '@/store'
-import 'element-plus/dist/index.css'
-import '@/style/index.css'
-
-createApp(App)
-    .use(store)
-    .mount('#app')
